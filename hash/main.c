4eb4e9562aa15029c2d3ac13ffcdcfe4290cb93f
津田準 <TsudaJun@tsudajun-no-MacBook-Air.local>
2017-12-10 17:21:48 +0900

    Initial Commit

---
+++ hash/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  hash
+//
+//  Created by 津田準 on 2017/12/10.
+//  Copyright © 2017 津田準. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
